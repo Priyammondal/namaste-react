6e7410526d40e463d227870fad59540aab80019e
Priyam Mondal <priyammondal096@gmail.com>
2023-10-16 21:36:05 +0530

    inception

---
+++ App.js
@@ -0,0 +1,57 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hellow World! from React"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("roort->", root);
+// root.render(heading);
+
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>This is a h1 tag!</h1>
+  </div>
+</div> */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "This is a h1 tag!")
+  )
+);
+// root.render(parent);
+
+console.log("parent-->", parent); // This is nothing but a object
+// so, React.createElement doesn't create the element, it creates a object which contains all the properties of the element,
+// ReactDom then convertes the object to actual element and place it into the DOM while rendering it.
+
+// If we want to create a sibling element, we need to put those childs inside an array
+
+{
+  /* <div id="parent">
+    <div id="child">
+      <h1>This is a h1 tag!</h1>
+      <h2>This is a h2 tag!</h2>
+    </div>
+  </div> */
+}
+
+const parent2 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is a h1 tag!"),
+    React.createElement("h2", {}, "This is a h2 tag!"),
+  ])
+);
+
+root.render(parent2);
+
+// if There is a tag already present in our html code, in our #root,
+// while rendering, that tag/those tags will be replaced ( not appended, html will first print it on the screen then it will be replaced ) by whatever we
+// will be passing inside our render method
