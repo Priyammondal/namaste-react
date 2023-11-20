a0e75461be03ab4a2cb6025ff4aeee38a1da1d1b
Priyammondal <priyammondal096@gmail.com>
2023-11-20 23:15:33 +0530

    episode-5 half

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,45 +1,21 @@
 import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => alert("Filter button clicked!")}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        <RestaurantCard
-          restaurant="Behrouz Biryani"
-          img="https://b.zmtcdn.com/data/dish_photos/77a/70033edf4e8ade8cab451efae1faa77a.jpg"
-          cuisine="Biryani, Kebab, Roll"
-          stars="4.3 stars"
-          time="22 min"
-        />
-        <RestaurantCard
-          restaurant="La Pino'z Pizza"
-          img="https://b.zmtcdn.com/data/pictures/chains/9/18438909/8ac67f799c14cd3b586e18e48eaa00f7_o2_featured_v2.jpg"
-          cuisine="Pizza, Pasta, Italian"
-          stars="4.1 stars"
-          time="24 min"
-        />
-        <RestaurantCard
-          restaurant="The Belgian Waffle Co."
-          img="https://b.zmtcdn.com/data/pictures/chains/1/18543621/bd6d35499c5fb33bea73c32bbf4b0237_o2_featured_v2.jpg"
-          cuisine="Waffle, Pancake, Ice Cream"
-          stars="4.2 stars"
-          time="30 min"
-        />
-        <RestaurantCard
-          restaurant="KFC"
-          img="https://b.zmtcdn.com/data/pictures/chains/5/112945/148024162061ada077592ebeaef52f89_o2_featured_v2.jpg"
-          cuisine="Burger, Fast Food, Fries"
-          stars="3.7 stars"
-          time="33 min"
-        />
-        <RestaurantCard
-          restaurant="Alinea Restaurant & Banquet"
-          img="https://b.zmtcdn.com/data/pictures/9/111899/b5003ed066467bc858298934f73c4574_o2_featured_v2.jpg"
-          cuisine="North Indian, Chinese"
-          stars="4.2 stars"
-          time="32 min"
-        />
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,15 +1,36 @@
+// to import a named export we use curly {} bracket
+// for default export we don't need any bracket
+
+import { CDN_URL } from "../utils/constants";
+
 const styleCard = {
   backgroundColor: "#f0f0f0",
 };
 
 const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+  } = resData?.data;
+
   return (
     <div className="res-card" style={styleCard}>
-      <img className="res-logo" src={props.img} alt="res-logo" />
-      <h3>{props.restaurant}</h3>
-      <h4>{props.cuisine}</h4>
-      <h4>{props.stars}</h4>
-      <h4>{props.time}</h4>
+      <img
+        className="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+        alt="res-logo"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>₹{costForTwo / 100} FOR TWO</h4>
+      <h4>{deliveryTime} minutes</h4>
     </div>
   );
 };
