0da186aa3cf754cbeb3b6dc4c4fe8ff463f903c0
Priyammondal <priyammondal096@gmail.com>
2023-12-07 00:10:01 +0530

    episode 6 done

---
+++ src/components/Shimmer.js
@@ -0,0 +1,22 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-btn-container">
+        <div className="first-btn"></div>
+        <div className="second-btn"></div>
+      </div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
